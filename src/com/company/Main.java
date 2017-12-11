66718d9fe694c1fd5c885014a2df88f02093f695
ls059 <lyspers@gmail.com>
2017-12-11 08:51:26 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean a, b;
+        for(int i = 0;i<4;i++){
+            if(i % 4 == 0)
+                a = true; b = true;
+            if(i % 4 == 1)
+                a = false; b = true;
+            if(i % 4 == 2)
+                a = true; b = false;
+            if(i % 4  == 3)
+                a = false; b = false;
+            
+
+
+
+
+
+
+        }
+
+
+    }
+}
